9882c519e478f562301afa31c82dd4f5eb2cf877
Антон <anton@workout.su>
2023-11-24 18:46:13 +0300

    Initial Commit

---
+++ FairPrice/FairPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairPriceApp.swift
+//  FairPrice
+//
+//  Created by Natalia D on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FairPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
